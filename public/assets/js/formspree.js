d5f878209dac498baf32cad09f306417b876504f
Callum Flack <callumflack@gmail.com>
2017-05-12 16:08:11 +1000

    styling & content wrap-up #1

--- public/assets/js/formspree.js
+++ public/assets/js/formspree.js
@@ -5,63 +5,63 @@ let defaultSubmitText = submitButton.textContent;
 
 
 function setFlashMessage(success) {
-	while (flashMessagesContainer.firstChild) {
-		flashMessagesContainer.removeChild(flashMessagesContainer.firstChild);
-	}
+    while (flashMessagesContainer.firstChild) {
+        flashMessagesContainer.removeChild(flashMessagesContainer.firstChild);
+    }
 
-	let messageElem = document.createElement('div');
-	let messageType = success ? 'success' : 'danger';
-	messageElem.classList.add('c-alert');
-	messageElem.classList.add(`c-alert-${messageType}`);
+    let messageElem = document.createElement('div');
+    let messageType = success ? 'success' : 'danger';
+    messageElem.classList.add('c-alert');
+    messageElem.classList.add(`c-alert-${messageType}`);
 
-	if (success) {
-		messageElem.innerHTML = `Message sent! Download the pdf <a class='o-linkUnderline' href='/download'>here</a>.`;
-	} else {
-		messageElem.innerHTML = 'Woops, something went wrong. Please try again.';
-	}
+    if (success) {
+        messageElem.innerHTML = `Sent! Download <a class='o-linkUnderline' href='/download'>three proven ways to make effective change</a>.`;
+    } else {
+        messageElem.innerHTML = 'Woops, something went wrong. Please try again.';
+    }
 
-	flashMessagesContainer.appendChild(messageElem);
+    flashMessagesContainer.appendChild(messageElem);
 }
 
 
 function submitForm(event) {
-	let xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
 
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 200) {
-				let response = JSON.parse(xhr.responseText);
-				setFlashMessage(true);
-			} else {
-				setFlashMessage(false);
-			}
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let response = JSON.parse(xhr.responseText);
+                setFlashMessage(true);
+            } else {
+                setFlashMessage(false);
+            }
 
-			submitButton.removeAttribute('disabled');
-			submitButton.textContent = defaultSubmitText;
-		}
-	};
+            submitButton.removeAttribute('disabled');
+            submitButton.textContent = defaultSubmitText;
+        }
+    };
 
-	let formData = {};
-	const acceptedTags = ['input', 'textarea'];
+    let formData = {};
+    const acceptedTags = ['input', 'textarea'];
 
-	[].map.call(contactForm.elements, (element) => {
-		const tagName = element.tagName.toLowerCase();
+    [].map.call(contactForm.elements, (element) => {
+        const tagName = element.tagName.toLowerCase();
 
-		if (acceptedTags.includes(tagName)) {
-			formData[element.name] = element.value;
-		}
-	});
+        if (acceptedTags.includes(tagName)) {
+            formData[element.name] = element.value;
+        }
+    });
 
-	xhr.open('POST', contactForm.action, true);
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.send(JSON.stringify(formData));
+    xhr.open('POST', contactForm.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(formData));
 }
 
 
 contactForm.addEventListener('submit', (event) => {
-	event.preventDefault();
+    event.preventDefault();
 
-	submitButton.setAttribute('disabled', 'true');
-	submitButton.textContent = 'Sending...';
-	submitForm();
+    submitButton.setAttribute('disabled', 'true');
+    submitButton.textContent = 'Sending...';
+    submitForm();
 });
